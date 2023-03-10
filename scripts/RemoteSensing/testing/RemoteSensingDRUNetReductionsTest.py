132e5fd6891040d0ddbbeda8ea2ac44af0e36078
Mathé Zeegers <zeegers@cwi.nl>
2023-03-10 13:45:40 +0100

    Fixed testing scripts descriptions

--- scripts/RemoteSensing/testing/RemoteSensingDRUNetReductionsTest.py
+++ scripts/RemoteSensing/testing/RemoteSensingDRUNetReductionsTest.py
@@ -3,8 +3,8 @@
 # This script carries out the testing of UNet networks on the remote sensing based testing data
 # The results are the csv files in the results/RemoteSensing/quantitative/ folder
 
-# The code assumes that the UNet networks are trained and available in the scripts/RemoteSensing/training/UNet/ManyMaterialsTrue folder
-# It also assumes the (testing) datasets to be available in the /data/RemoteSensingDatasets/ManyMaterialsTrue/ folder
+# The code assumes that the UNet networks are trained and available in the scripts/RemoteSensing/training/UNet/PartiallyOverlapping(True/False) folders
+# It also assumes the (testing) datasets to be available in the /data/RemoteSensingDatasets/PartiallyOverlapping(True/False)/ folders
 
 #Author,
 #   Mathé Zeegers, 
