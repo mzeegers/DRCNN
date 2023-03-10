82d18172141497b77f39d952648d5a6c9b8c4cab
Mathé Zeegers <zeegers@cwi.nl>
2023-03-10 15:00:09 +0100

    Added scripts for the generation of the (spatial) remote sensing configurations

---
+++ scripts/RemoteSensing/generation/EnvironmentsGeneratorPartiallyOverlapping.py
@@ -0,0 +1,132 @@
+#Generation of remote sensing configurations with overlapping elements
+
+#This script carries out the generation of remote sensing configurations which consist of cylinders of various shapes that can overlap each other
+# (there are two groups of cylinders have the possibility to overlap cylinders from the other group, but not those of their own group. The ratio between the groups is 1:5)
+#The results are a stack of images with one cylinder, which can be found in data/RemoteSensingDatasets/RemoteSensingConfigurations/
+
+#Authors,
+#   Mathé Zeegers, 
+#       Centrum Wiskunde & Informatica, Amsterdam (m.t.zeegers@cwi.nl)
+
+import numpy as np
+import os
+import random as rd
+import scipy.ndimage
+import tifffile
+import random
+
+np.set_printoptions(threshold=np.inf)
+
+os.makedirs('../../../data/RemoteSensingDatasets/', exist_ok=True)
+os.makedirs('../../../data/RemoteSensingDatasets/RemoteSensingConfigurationsPartiallyOverlapping/', exist_ok=True)
+
+#Insert given object Obj in given array Field with a given value (Assumption: Field is square (SIZE is size of the world))
+def insertObject(Obj, centery, centerx, radius, value, Field, SIZE, checkFrame = False, AllowList = [0]):
+    Obj[Obj > 0] = value
+
+    #determine copy ranges
+    minyField = max(0, centery-radius)
+    maxyField = min(SIZE, centery+radius+1)
+    minxField = max(0, centerx-radius)
+    maxxField = min(SIZE, centerx+radius+1)
+
+    minyObj = max(0, radius - centery)
+    maxyObj = min(2*radius+1, radius - centery + SIZE)
+    minxObj = max(0, radius - centerx)
+    maxxObj = min(2*radius+1, radius - centerx + SIZE)
+
+    ObjSlice = Obj[minyObj:maxyObj,minxObj:maxxObj]    
+
+    FieldRange = Field[minyField:maxyField,minxField:maxxField]
+
+    #Check whether position is already occupied
+    if checkFrame == True:
+        if np.all(np.in1d(FieldRange[ObjSlice!=0], AllowList)) == False:
+            print("Object cannot be placed: space already occupied by other object")
+            return False
+
+    #Insert the object
+    Field[minyField:maxyField,minxField:maxxField][ObjSlice > 0] = ObjSlice[ObjSlice>0]
+
+    print("Inserted object at location = (y,x)", centery, centerx)
+    return True
+
+
+WSize = 3072        #Size of the image
+Rsf = 6             #Factor to downsize the final image with
+NObj = 360          #Number of cylinders for each instance
+Ninstances = 100    #Number of instances to create
+
+for inst in range(0, Ninstances):
+
+    World = np.zeros((NObj,WSize,WSize))
+    WorldSum = np.zeros((WSize,WSize))
+    WorldSumForeground = np.zeros((WSize,WSize))
+    WorldSumBackground = np.zeros((WSize,WSize))
+
+    np.random.seed(inst)
+    random.seed(inst)
+
+    Obj = 0
+    while Obj < NObj:
+        print(Obj)
+
+        #l:    length of the tube
+        #a:    radius of the tube
+        #ang:  tilting (in degrees)
+        l = rd.randint(46,468)
+        a = rd.randint(15,46)
+        ang = rd.randint(0,180)
+
+        maxHalfSize = np.amax([a,a+int((l+1)/2)])
+        maxHalfSizeTube = int((l+1)/2)
+        Frame = np.zeros((2*maxHalfSize+1,2*maxHalfSize+1))
+
+        yy,xx = np.mgrid[-maxHalfSize:maxHalfSize+1,-maxHalfSize:maxHalfSize+1]
+
+        #Make cylinder
+        Dist = yy**2
+
+        Frame[Dist < a**2] = 1
+
+        Frame[xx > maxHalfSizeTube] = 0
+        Frame[xx < -maxHalfSizeTube] = 0
+
+        #Make cones on top
+        Dist1 = (yy**2+(xx-maxHalfSizeTube)**2)
+        Frame[Dist1 < a**2] = 1
+        Dist2 = (yy**2+(xx+maxHalfSizeTube)**2)
+        Frame[Dist2 < a**2] = 1
+
+        #Rotate according to given angles
+        Frame = scipy.ndimage.rotate(Frame, ang, axes=(0,1), reshape = False)
+
+        Frame[Frame >= 0.5] = 1
+        Frame[Frame <= 0.5] = 0
+
+        #Select random location
+        py = rd.randint(maxHalfSize,WSize-maxHalfSize)
+        px = rd.randint(maxHalfSize,WSize-maxHalfSize)
+        
+        #Insert the cylinder
+        Inserted = False
+        if(Obj % 6 == 0):
+            Inserted = insertObject(Frame, py, px, maxHalfSize, 1, WorldSumForeground, WSize, True)
+        else:
+            Inserted = insertObject(Frame, py, px, maxHalfSize, 1, WorldSumBackground, WSize, True)
+        
+        if(Inserted == True):
+            insertObject(Frame, py, px, maxHalfSize, 1, WorldSum, WSize, False)
+            insertObject(Frame, py, px, maxHalfSize, 1, World[Obj,:,:], WSize, False)
+            Obj = Obj+1
+
+    ###Resizing the data by factor Rsf
+    RsS = int(World.shape[-1]/Rsf)
+    Resize = np.zeros((World.shape[0],RsS,RsS))
+    for resi in range(Rsf):
+        for resj in range(Rsf):
+            Resize += World[:,resi::Rsf,resj::Rsf]
+    Result = (Resize/(Rsf*Rsf)).astype(np.float32)
+
+    #Save the result
+    tifffile.imsave('../../../data/RemoteSensingDatasets/RemoteSensingConfigurationsPartiallyOverlapping/Instance' + str(inst).zfill(3) + '.tiff', Result.astype(np.float32))
